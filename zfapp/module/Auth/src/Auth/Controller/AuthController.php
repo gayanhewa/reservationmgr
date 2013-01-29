d3e781359038aef771021db2a07c2a59cde20ef5
Gayan <gayanhewa@gmail.com>
2013-01-29 15:40:34 +0530

    Initialize the auth module

---
+++ zfapp/module/Auth/src/Auth/Controller/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+namespace Auth\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AuthController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        die('eee');
+    }
+
+    public function addAction()
+    {
+        die('add');
+    }
+
+    public function editAction()
+    {
+        die('edit');
+    }
+
+    public function deleteAction()
+    {
+        die('delete');
+    }
+}
